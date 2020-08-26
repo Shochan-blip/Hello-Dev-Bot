e8bf94375a9dba77ebdb548d1a4a9740e1d10ba7
Moosemoosetwutwu <70246651+Moosemoosetwutwu@users.noreply.github.com>
2020-08-26 15:30:40 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = ':';
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+    for(const file of commandFiles){
+        const command = require(`./commands/${file}`);
+ 
+        client.commands.set(command.name, command);
+
+    }
+
+client.once('ready', () => {
+    console.log('HelloBot is Online!');
+});
+
+
+
+
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong!');
+    }
+});
+
+
+client.login('NzQ3NjY5NjgyNjQyODc4NTA1.X0SPpw.VzAvWUXDKagPduLEfzMJU1scE28');
